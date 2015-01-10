067cc23589505514857f9aadec811bf65df7f108
ryan <ilmaestro@gmail.com>
2015-01-09 22:01:28 -0800

    added Arcadia asset

---
+++ throwaway/unity-test-01/Assets/Arcadia/Scripts/hello/hellounity.clj
@@ -0,0 +1,8 @@
+;; hello unity example initial testing.
+(ns hello.hellounity
+	(:use arcadia.core)
+	(:import [UnityEngine Application]))
+
+; (defcomponent Hello [^float speed]
+; 	(Update [this]
+; 		(.. this transform) position x))
