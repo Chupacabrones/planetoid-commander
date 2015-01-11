192345ed70dbbbe5e575d34ccf3f32749e479cd6
ryan <ilmaestro@gmail.com>
2015-01-10 21:05:53 -0800

    more working code with live repl

--- throwaway/unity-test-01/Assets/Arcadia/Scripts/hello/hellounity.clj
+++ throwaway/unity-test-01/Assets/Arcadia/Scripts/hello/hellounity.clj
@@ -1,7 +1,7 @@
 ;; hello unity example initial testing.
 (ns hello.hellounity
 	(:use arcadia.core)
-	(:import [UnityEngine Application]))
+	(:import [UnityEngine Input Application Debug Vector3]))
 
 (declare move-update)
 
@@ -9,19 +9,34 @@
 	(Update [this]
 		(move-update this)))
 
+(defcomponent LightDisabler []
+	(Update [this]
+		(let [light (get-component this "Light")]
+			(if (.. Input (GetKeyUp "space"))
+				(set! (.. light enabled) false)))))
+
 (defn move-update [^MoveSomething this]
-	(.. this transform (Translate (.speed this) 0 0)))
+	(.. this transform (Translate (* 
+									(.speed this) 
+									Time/deltaTime)
+								   0 
+								   0))
+	;(Debug/Log "calling update..")
+	(if (< (.. this transform position x)
+			-2)
+		(set! (.. this transform position)
+			(Vector3. 4 0 0))))
 
 (defn reset-world []
 	(Application/LoadLevel Application/loadedLevel))
 
-(defn do-stuff[]
+(defn do-stuff []
 	(reset-world)
-	;; this isn't working right now... 
 	(set! (.. (object-named "Cube")
 		  (GetComponent hello.hellounity.MoveSomething)
 		  speed)
-	  (float 0.1)))
+	  (float -6))
+	)
 
 
 
