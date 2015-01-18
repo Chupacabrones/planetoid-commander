77b42fdf0481e31b06b04dfeb6073dcedd94b412
ryan <ilmaestro@gmail.com>
2015-01-18 13:36:12 -0800

    some more working code

--- throwaway/unity-test-01/Assets/Arcadia/Scripts/planetoid/cubes.clj
+++ throwaway/unity-test-01/Assets/Arcadia/Scripts/planetoid/cubes.clj
@@ -1,26 +1,31 @@
 ;; cube testing in unity
 (ns planetoid.cubes
-	(:use arcadia.core)
+	(:use arcadia.core
+		  planetoid.vectormath)
 	(:import [UnityEngine Input Application Debug Vector3]))
-
-(declare clamp-magnitude)
-(declare velocity-to-target)
-(declare square)
 
 ;; class vars??
 (def cube1 (object-named "Cube1"))
-(def max-move-distance (float 500.0))
+(def max-move-distance (float 10.0))
 
 ;; orbit component definition
 (defcomponent Orbit [^float speed]
+	(Awake [this]
+		;; add an initial force in some direction
+		(doto (.. this rigidbody)
+			(.AddForce (Vector3. 0 100 10))))
+
 	(FixedUpdate [this]
 		(let [moveDistance (* (float max-move-distance) Time/deltaTime)
 			  sourcePos (.. this transform position)
 			  targetPos (.. cube1 transform position)
-			  velocityVec (clamp-magnitude 
-			  				(- (velocity-to-target sourcePos targetPos (float moveDistance)) 
-			  				   (.. this rigidbody velocity))
-			  				 moveDistance)]
+			  velTargetVec (v-seek 
+			  				  sourcePos 
+			  				  targetPos 
+			  				  (float moveDistance))
+			  velocityVec  (v-clamp 
+			  				 (v- velTargetVec (.. this rigidbody velocity))
+			  				  moveDistance)]
 				;; move position
 				;(set! (.. this transform position) (Vector3. 0 0 0))
 				
@@ -28,27 +33,14 @@
 				;(.. this transform (Translate 0 0 0))
 				
 				;; LookAt cube 1
-				(.. this transform (LookAt (.. cube1 transform)))
+				(doto (.. this transform)
+					(.LookAt (.. cube1 transform)))
 
 				;; add force to the rigidBody
-				(.. this rigidbody (AddForce velocityVec))
-			  )
-	
-	 ))
+				(doto (.. this rigidbody)
+					(.AddForce velocityVec))
+				)))
 
-(defn clamp-magnitude [^Vector3 the-vector max-magnitude]
-	(if (> (.. the-vector magnitude)
-			(float max-magnitude))
-		(* (.. the-vector normalized) (float max-magnitude))
-		the-vector))
-
-(defn velocity-to-target ^Vector3 [^Vector3 source ^Vector3 target move-distance]
-	(let [directionToTarget (.. Vector3 (Normalize (- target source)))]
-		   (v* (float move-distance) directionToTarget)))
-
-
-(defn square [x]
-	(float (* x x)))
 
 
 
--- throwaway/unity-test-01/Assets/Arcadia/Scripts/planetoid/vectormath.clj
+++ throwaway/unity-test-01/Assets/Arcadia/Scripts/planetoid/vectormath.clj
@@ -1,13 +1,78 @@
 (ns planetoid.vectormath
 	(:import [UnityEngine Vector3]))
 
-;; ==============
-;; vector math
-;; ==============
-
-(defn v3 ^Vector3 [x y z]
-	(Vector3. x y z))
-
+;; ===================
+;; vector math helpers
+;; ===================
 
 (defn v- ^Vector3 [^Vector3 v1 ^Vector3 v2]
 	(Vector3/op_Subtraction v1 v2))
+
+(defn v+ ^Vector3 [^Vector3 v1 ^Vector3 v2]
+	(Vector3/op_Addition v1 v2))
+
+(defn v* ^Vector3 [^Vector3 v d]
+	(Vector3/op_Multiply v (float d)))
+
+(defn v÷ ^Vector3 [^Vector3 v1 ^Vector3 v2]
+	(Vector3/op_Division v1 v2))
+
+;; clamp a vectors magnitude by a maximum value
+(defn v-clamp ^Vector3 [^Vector3 v clamp]
+	(if (> (.. v magnitude)
+		   (float clamp))
+		;; clamp
+		(v* (.. v normalized) (float clamp))
+		;; don't clamp
+		 v))
+
+(defn v-normalize ^Vector3 [^Vector3 v]
+	(.. Vector3 (Normalize v)))
+
+(defn v-seek ^Vector3 [^Vector3 source ^Vector3 target move-distance]
+	(let [directionToTarget (v-normalize (v- target source))]
+		   (v* directionToTarget (float move-distance))))
+
+(defn v-test-01 []
+	(let [v1 (Vector3. 1 1 1)
+		  v2 (Vector3. 2 2 2)]
+		(v- v1 v2)))
+
+(defn v-test-02 []
+	(let [v1 (Vector3. 1 1 1)
+		  v2 (Vector3. 2 2 2)]
+		(v+ v1 v2)))
+
+(defn v-test-03 []
+	(let [v1 (Vector3. 1 1 1)
+		  d (float 2.0)]
+		(v* v1 d)))
+
+(defn v-test-04 []
+	(let [v1 (Vector3. 1 1 1)
+		  v2 (Vector3. 2 2 2)]
+		(v÷ v1 v2)))
+
+(defn v-test-05 []
+	(let [v1 (Vector3. 3 1 5)]
+		(v-clamp v1 1.0)))
+
+(defn v-test-06 []
+	(let [v1 (Vector3. 3 1 5)]
+		(v-normalize v1)))
+
+(defn v-test-07 []
+	(let [v1 (Vector3. 1 1 1)
+		  v2 (Vector3. 2 2 2)
+		  d (float 1.0)]
+		(v-seek v1 v2 d)))
+
+(defn run-tests []
+	(v-test-01)
+	(v-test-02)
+	(v-test-03)
+	;(v-test-04)
+	(v-test-05)
+	(v-test-06)
+	(v-test-07)
+	)
