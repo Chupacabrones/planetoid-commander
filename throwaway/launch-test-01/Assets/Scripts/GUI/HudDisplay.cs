53915ba483cce6ab76f6d399cd0d465899961fe4
Ryan Kilkenny <ilmaestro@gmail.com>
2015-02-26 22:45:14 -0800

    added new Launch scene with crude HUD/Launcher

---
+++ throwaway/launch-test-01/Assets/Scripts/GUI/HudDisplay.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HudDisplay : MonoBehaviour {
+
+    public RectTransform UIPanel;
+    public Canvas UICanvas;
+
+    private RectTransform CanvasRect;
+
+	// Use this for initialization
+	void Awake () {
+        CanvasRect = UICanvas.GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    Vector2 ScreenPositionOfWorldObject(Vector3 worldPosition, RectTransform CanvasRect)
+    {
+        //then you calculate the position of the UI element
+        //0,0 for the canvas is at the center of the screen, whereas WorldToViewPortPoint treats the lower left corner as 0,0. Because of this, you need to subtract the height / width of the canvas * 0.5 to get the correct position.
+        Vector2 ViewportPosition = Camera.main.WorldToViewportPoint(worldPosition);
+        Vector2 WorldObject_ScreenPosition = new Vector2(
+        ((ViewportPosition.x * CanvasRect.sizeDelta.x) - (CanvasRect.sizeDelta.x * 0.5f)),
+        ((ViewportPosition.y * CanvasRect.sizeDelta.y) - (CanvasRect.sizeDelta.y * 0.5f)));
+        return WorldObject_ScreenPosition;
+    }
+
+    public void SetHudAtWorldPosition(Vector3 worldPosition)
+    {
+        UIPanel.anchoredPosition = ScreenPositionOfWorldObject(worldPosition, CanvasRect);
+    }
+}
---
+++ throwaway/launch-test-01/Assets/Scripts/Launcher/Selector.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum SelectionState
+{
+    Unselected,
+    Selected,
+    Launching,
+    Launched
+}
+
+public class Selector : MonoBehaviour {
+
+    public GameObject HUD;
+
+    private SelectionState selectionState = SelectionState.Unselected;
+    private HudDisplay hudDisplay;
+    private Color c1 = Color.yellow;
+    private Color c2 = Color.red;
+
+    private int boardMask;
+    private float maxLaunchDistance = 10.0f;
+    private Vector3 lastLaunchPoint;
+
+    void Awake()
+    {
+        boardMask = LayerMask.GetMask("BoardLayer");
+        hudDisplay = HUD.GetComponent<HudDisplay>();
+        HUD.SetActive(false);
+    }
+
+    void Start()
+    {
+        LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+        lineRenderer.SetColors(c1, c2);
+        lineRenderer.SetWidth(0.2F, 0.2F);
+        lineRenderer.SetVertexCount(2);
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.enabled = false;
+    }
+
+	void Update () {
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+
+        switch (selectionState)
+        {
+            case SelectionState.Unselected:
+                // check for mouse button
+                if (Input.GetButtonUp("Fire1"))
+                {
+                    //cast ray from mouseposition 
+                    Debug.Log("Mouse clicked");
+                    Ray mouseray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    RaycastHit hit;
+                    //check if the ray hits a Player object
+                    if (Physics.Raycast(mouseray, out hit))
+                    {
+                        Debug.Log("Hit something.. " + hit.transform.tag);
+                        if (hit.transform.tag == "Player")
+                        {
+                            //bring up the launch menu
+                            selectionState = SelectionState.Selected;
+                            hudDisplay.SetHudAtWorldPosition(hit.transform.position);
+                        }
+                    }
+                }
+                break;
+            case SelectionState.Selected:
+
+                if (Input.GetButton("Fire1"))
+                {
+                    //Draw a ray from center of gameObject to mousePosition
+                    lineRenderer.enabled = true;
+                    Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    RaycastHit boardHit;
+                    if (Physics.Raycast(camRay, out boardHit, boardMask))
+                    {
+                        lastLaunchPoint = boardHit.point;
+                        lineRenderer.SetPosition(1, lastLaunchPoint);
+                    }
+                }
+
+                if (Input.GetButtonUp("Fire1"))
+                {
+                    selectionState = SelectionState.Launching;
+                    LaunchSomething(lastLaunchPoint);
+                }
+                break;
+            case SelectionState.Launching:
+                selectionState = SelectionState.Unselected;
+                lineRenderer.enabled = false;
+                break;
+            default:
+                break;
+        }
+
+        HUD.SetActive(selectionState == SelectionState.Selected);
+	}
+    void LaunchSomething(Vector3 launchVector)
+    {
+        Debug.Log("Launched from: " + launchVector.ToString());
+    }
+}
